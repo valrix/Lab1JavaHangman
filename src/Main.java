05d8646231a81d9b3722014d91812816d30b73c4
valrix <55671196+valrix@users.noreply.github.com>
2020-10-04 21:10:36 -0400

    Added a feature that selects a random word from the list, splits it letter for letter and populates a separate ArrayList and prints the blanks to the screen

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,30 @@ public class Main {
         wordBank.add("watermelon");
         wordBank.add("pineapple");
         wordBank.add("cantaloupe");
+
+        // Create a Random object to choose a random word from the list
+        Random randomWordIndex = new Random();
+
+        // Assign the random word by choosing a random index from the word bank list
+        String randomWord = wordBank.get(randomWordIndex.nextInt(wordBank.size()));
+
+        // Create an ArrayList that will hold each letter of the random word as it's own index
+        ArrayList<String> randomWordSplitArrayList = new ArrayList<>();
+
+        // Add each letter of the random word to the ArrayList by using the Collections class
+        Collections.addAll(randomWordSplitArrayList, randomWord.split(""));
+
+        // Create a separate ArrayList that will hold the blank representations of each letter in the word
+        ArrayList<String> hiddenWordArrayList = new ArrayList<>();
+
+        // Populate the hiddenWordArrayList with the same amount of blanks as the length of the random word
+        for (int i = 0; i < randomWord.length(); i++) {
+            hiddenWordArrayList.add("_ ");
+        }
+
+        // Loop through the list and display the blanks to the user as the start of the game
+        for (String blank : hiddenWordArrayList) {
+            System.out.print(blank + " ");
+        }
     }
 }
